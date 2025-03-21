f04103f2ddda75730304564f3da463913b0ca724
AHorejsi <alex.horejsi59@gmail.com>
2025-03-21 13:50:52 -0700

    Implemented text for failed login

--- src/scripts/LoginPage.tsx
+++ src/scripts/LoginPage.tsx
@@ -1,28 +1,45 @@
-import React, { ReactNode, useState } from "react";
+import "../styles/LoginPage.scss";
+import React, { Dispatch, ReactNode, SetStateAction, useState } from "react";
 import { NavigateFunction, useNavigate } from "react-router";
 import { login } from "./Fetch";
 import { LoginInfo } from "./LoginInfo";
 
-function _checkLogin(info: LoginInfo, nav: NavigateFunction) {
+interface _LoginAttemptState {
+    borders: string;
+
+    text: string;
+
+    color: string;
+}
+
+function _checkLogin(info: LoginInfo, setLogin: Dispatch<SetStateAction<_LoginAttemptState>>, nav: NavigateFunction) {
     const user = info.user;
 
     if (!user) {
-        throw new Error("Unable to login");
+        setLogin({ borders: "failed-login" , text: "Username or Password not authenticated", color: "failed-login-text" });
     }
+    else {
+        const options = {
+            state: user,
+            replace: false
+        };
     
-    const options = {
-        state: user,
-        replace: false
-    };
-
-    nav("/gameplay", options);
+        nav("/gameplay", options);
+    }
 }
 
-function _attemptUserLogin(usernameOrEmail: string, password: string, nav: NavigateFunction) {
+function _attemptUserLogin(
+    usernameOrEmail: string,
+    password: string,
+    setLogin: Dispatch<SetStateAction<_LoginAttemptState>>,
+    nav: NavigateFunction
+) {
+    setLogin({ borders: "login-not-attempted", text: "Authenticating...", color: "login-text" });
+
     const loginResult = login(usernameOrEmail, password);
 
     loginResult.then((info: LoginInfo) => {
-        _checkLogin(info, nav);
+        _checkLogin(info, setLogin, nav);
     }).catch((error: Error) => {
         throw error;
     });
@@ -31,29 +48,48 @@ function _attemptUserLogin(usernameOrEmail: string, password: string, nav: Navig
 export default function LoginPage(): ReactNode {
     const [usernameOrEmail, setUsernameOrEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    const [login, setLogin] = useState<_LoginAttemptState>({ borders: "login-not-attempted", text: "", color: "" });
     
     const nav = useNavigate();
 
     return (
-        <div id="login">
+        <div id="login" className="container-fluid">
             <form>
+                <p className={login.color}>{login.text}</p>
+
                 <div>
                     Username/Email: 
 
                     <label htmlFor="usernameOrEmail" />
-                    <input type="text" name="usernameOrEmail" onInput={(ev) => setUsernameOrEmail(ev.currentTarget.value)} />
+                    <input
+                        className={login.borders}
+                        type="text"
+                        name="usernameOrEmail"
+                        onInput={(ev) => setUsernameOrEmail(ev.currentTarget.value)}
+                    />
                 </div>
 
                 <div>
                     Password: 
 
                     <label htmlFor="password" />
-                    <input type="password" name="password" onInput={(ev) => setPassword(ev.currentTarget.value)} />
+                    <input
+                        className={login.borders}
+                        type="password"
+                        name="password"
+                        onInput={(ev) => setPassword(ev.currentTarget.value)}
+                    />
                 </div>
 
                 <div>
                     <label htmlFor="login" />
-                    <input type="button" name="login" value="Login" onClick={(_) => _attemptUserLogin(usernameOrEmail, password, nav) } />
+                    <input
+                        type="button"
+                        name="login"
+                        value="Login"
+                        onClick={(_) => _attemptUserLogin(usernameOrEmail, password, setLogin, nav) }
+                    />
                 </div>
             </form>
         </div>
