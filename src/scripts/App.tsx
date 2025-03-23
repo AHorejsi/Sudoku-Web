869dd06ec554de81bef94122c2954c846bc29dae
AHorejsi <alex.horejsi59@gmail.com>
2025-03-23 10:53:20 -0700

    Started on main page

--- src/scripts/App.tsx
+++ src/scripts/App.tsx
@@ -4,11 +4,13 @@ import GameplayPage from "./GameplayPage";
 import LoginPage from "./LoginPage";
 import SignUpPage from "./SignUpPage";
 import UserSettingsPage from "./UserSettingsPage";
+import MainPage from "./MainPage";
 
 export default function App(): ReactNode {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<MainPage />} />
                 <Route path="/signup" element={<SignUpPage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/gameplay" element={<GameplayPage />} />
---
+++ src/scripts/MainPage.tsx
@@ -0,0 +1,17 @@
+import "../styles/MainPage.scss";
+import React, { ReactNode } from "react";
+import { useNavigate } from "react-router";
+
+export default function MainPage(): ReactNode {
+    const nav = useNavigate();
+
+    return (
+        <div id="top-card" className="container">
+            <button className="btn btn-primary" onClick={(_) => nav("/login")}>Login</button>
+
+            <span id="divider" />
+
+            <button className="btn btn-primary" onClick={(_) => nav("/signup")}>Sign Up</button>
+        </div>
+    );
+}
