06f2cddb0caeab7c18cbc83c3f48f63c3b94af1e
AHorejsi <alex.horejsi59@gmail.com>
2026-04-20 15:28:50 -0700

    Changing Auth Resources

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -3,16 +3,6 @@ import { defineAuth } from "@aws-amplify/backend";
 
 export const auth = defineAuth({
   loginWith: {
-      email: true,
-  },
-  userAttributes: {
-      email: {
-          mutable: true,
-          required: true
-      },
-      preferredUsername: {
-          mutable: true,
-          required: false
-      }
+      email: true
   }
 });
